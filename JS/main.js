5970468b58b935365c72cb6348ee37ec4db67e7a
ValerioMik <valerio.avallone@hotmail.com>
2021-11-15 11:57:41 +0100

    macro struttura commentata + pseudo codice

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+/* Milestone 1
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+nome e immagine di ogni contatto
+ */
+
+
+//1.imposto un layout di partenza su HTML
+//2.creo un arrey di oggetti contenenti nome,immagine,condizione true, 
+    //2.1 ed gli eventuali messaggi di partenza(primo giorno scritti a mano nell'html),
+    //2.2 faccio un ciclo v-for che mi scorra gli oggetti 
+        //2.3  stampo immagine e nome sull'html
