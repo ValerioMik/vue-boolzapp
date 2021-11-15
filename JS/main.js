48bd0afcf8059e92e622b8878213e4770f9a5453
ValerioMik <valerio.avallone@hotmail.com>
2021-11-15 20:21:33 +0100

    primo step quasi completato

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,98 @@ nome e immagine di ogni contatto
 
 //1.imposto un layout di partenza su HTML
 //2.creo un arrey di oggetti contenenti nome,immagine,condizione true, 
-    //2.1 ed gli eventuali messaggi di partenza(primo giorno scritti a mano nell'html),
-    //2.2 faccio un ciclo v-for che mi scorra gli oggetti 
-        //2.3  stampo immagine e nome sull'html
+//2.1 ed gli eventuali messaggi di partenza(primo giorno scritti a mano nell'html),
+//2.2 faccio un ciclo v-for che mi scorra gli oggetti 
+//2.3  stampo immagine e nome sull'html
+var app = new Vue(
+    {
+        el: '#app',
+        data: {
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: 'img/avatar_1.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: 'img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: 'img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Luisa',
+                    avatar: 'img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+            ]
+        }
+    }
+);
